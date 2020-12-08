340641308199bf0e0b7ec9f1137b9028aa24bf9f
Miguel Coccaro <mecoccaro.16@est.ucab.edu.ve>
2020-12-08 11:52:20 -0400

    Agregado footer

--- client/src/components/navbar.vue
+++ client/src/components/navbar.vue
@@ -1,17 +1,12 @@
 <template>
-  <div class="navbar">
-    <nav>
-      <div>
-        <img id="logo-nav" src="../assets/visa-logo.png" align="left">
-      </div>
-      <div>
-        <router-link class="boton1" to="/principal"><strong>Vista principal</strong></router-link>
-      </div>
-      <div align="right">
-        <a id="support" class="texts">Support</a>
-      </div>
-
-    </nav>
+  <div class="topnav">
+    <img id="logo-nav" src="../assets/visa-logo.png" align="left">
+    <div class="botones-nav">
+      <router-link class="active" to="/principal"><strong>Vista principal</strong></router-link>
+      <a href="#news">News</a>
+      <a href="#contact">Contact</a>
+      <a href="#about">About</a>
+    </div>
   </div>
 </template>
 
@@ -22,23 +17,38 @@ export default {
 </script>
 
 <style scoped>
+.topnav {
+  overflow: hidden;
+  background: linear-gradient(90deg, rgba(150,118,238,1) 0%, rgba(122,56,200,1) 35%, rgba(140,7,246,1) 100%);
+  padding-bottom: 50px;
+}
+
+.topnav a {
+  float: left;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+.topnav a:hover {
+  background-color: #875fc4;
+  color: black;
+}
+
+.topnav a.active {
+  background-color: #340a60;
+  color: white;
+}
 
 #logo-nav {
   width: 100px;
   justify-content: center;
 }
-.navbar{
-  margin-bottom: 10%;
-}
-.texts {
-  font-family: Tahoma;
-  color: white;
-  margin: auto;
-  width: 50%;
-  padding: 10px;
-}
-#support{
-  margin-right: 50px;
+
+.botones-nav{
+  float: right;
 }
 
 a.boton1{
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -13,8 +13,14 @@
     <div style="margin-top: 20px">
       <tabscenter></tabscenter>
     </div>
-    <div style="margin-top: 30px">
+    <div>
+      <purchase></purchase>
+    </div>
+    <div style="margin-top: 30px; padding-bottom: 100px">
       <carousel></carousel>
+    </div>
+    <div>
+      <footerbar/>
     </div>
   </div>
 
@@ -26,6 +32,8 @@ import toptext from "@/components/toptext";
 import card from "@/components/card";
 import tabscenter from "@/components/tabscenter";
 import carousel from "@/components/carousel";
+import purchase from "@/components/purchase";
+import footerbar from "@/components/footerbar";
 
 export default {
   name: 'Home',
@@ -34,16 +42,22 @@ export default {
     toptext,
     card,
     tabscenter,
-    carousel
+    carousel,
+    purchase,
+    footerbar
   }
 }
 </script>
 <style>
+body{
+  margin: 0px;
+}
 .top{
   border-top-color: rgb(150,118,238);
   background: rgb(150,118,238);
   background: linear-gradient(90deg, rgba(150,118,238,1) 0%, rgba(122,56,200,1) 35%, rgba(140,7,246,1) 100%);
   border-radius: 0 0 35% 35%;
+
 
 }
 
